dbe8ac1a39abbb794e408dff756980c64107f5ff
Sergei Vorfolomeev <vorfo1897@gmail.com>
2023-05-22 21:35:32 +0200

    refactor, add creation time, add filtration buttons

--- src/app/features/todos/todos.module.ts
+++ src/app/features/todos/todos.module.ts
@@ -6,9 +6,18 @@ import { FormsModule } from '@angular/forms'
 import { TodoComponent } from './components/todo/todo.component'
 import { TasksComponent } from './components/todo/tasks/tasks.component'
 import { TaskComponent } from './components/todo/tasks/task/task.component'
+import { FilterComponent } from './components/todo/filter/filter.component'
+import { FooterComponent } from './components/todo/footer/footer.component'
 
 @NgModule({
-  declarations: [TodosComponent, TodoComponent, TasksComponent, TaskComponent],
+  declarations: [
+    TodosComponent,
+    TodoComponent,
+    TasksComponent,
+    TaskComponent,
+    FilterComponent,
+    FooterComponent,
+  ],
   imports: [CommonModule, TodosRoutingModule, FormsModule],
 })
 export class TodosModule {}
